4f277b2078b2cb05c30922365943e7ffebe15040
venkatesh sirivella <sirivellavenkatesh636@gmail.com>
2024-09-21 10:26:30 +0530

    Navigation bar and footer completed

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Navigation'
+import Footer from './Footer'
+
+function Home() {
+  return (
+    <div>
+        <Navigation/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
